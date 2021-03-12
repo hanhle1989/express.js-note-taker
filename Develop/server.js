3d07e9ac304baf6fa8b7262ce8ac503b88c48772
Hanh Le <hanhdtle@gmail.com>
2021-03-11 18:57:04 -0800

    added routes

--- Develop/server.js
+++ Develop/server.js
@@ -1,12 +1,13 @@
 const express = require("express");
-const path = require('path');
 const app = express ();
 const PORT = process.env.PORT || 5000;
 
-app.use(express.static(__dirname + "./public/"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
 app.use(express.json());
 
-app.get("/api/notes", (req,res)=>res.json(notes));       
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);  
 
 
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
