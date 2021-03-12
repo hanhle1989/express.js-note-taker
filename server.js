98e2a515aedaeac0b51fcf0277e2b2c9bb589791
Hanh Le <hanhdtle@gmail.com>
2021-03-11 20:15:17 -0800

    changed directory in server to connect with develop folder

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const app = express ();
+const dbJson = require("./Develop/db/db.json");
 const PORT = process.env.PORT || 5000;
 
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static("public"));
+app.use(express.static(__dirname + "./Develop/public"));
 app.use(express.json());
 
-require("./develop/routes/apiRoutes")(app);
-require(".develop/routes/htmlRoutes")(app);  
+require("./Develop/routes/apiRoutes")(app);
+require(".Develop/routes/htmlRoutes")(app);  
 
 
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
