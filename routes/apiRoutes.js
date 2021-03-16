2d6768bbde68d7a2e8fe93cdcd133b3f601c7eb8
Hanh Le <hanhdtle@gmail.com>
2021-03-15 20:00:45 -0700

    FINALLY fixed the save button!

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,18 +1,14 @@
 // const dbJSON = require('./db/db.json');
 const fs = require('fs');
-
+// const uuid using npm uuid
 const { v4: uuidv4 } = require('uuid');
 
 //API Routing: 
 module.exports = (app) => {
     app.get('/api/notes', (req, res) => {
-        fs.readFile('./db/db.json', (err, data) => {
-            if (err) {
-                console.log(err);
-            } else {
-                res.json(JSON.parse(data));
-                console.log("New note has been saved");
-            }
+        fs.readFile('./db/db.json', 'utf8', (err, data) => {
+            if (err) throw err;
+            res.json(JSON.parse(data));
         });
     });
 
@@ -22,15 +18,16 @@ module.exports = (app) => {
         fs.readFile('./db/db.json', (err, data) => {
             if (err) {
                 console.log(err);
+            }
+            else {
                 let notes = JSON.parse(data);
-                notes.push(notesTaken);
+                notes.push(newNote);
                 fs.writeFile('./db/db.json', JSON.stringify(notes), (err) => {
-                    if (err) {
-                        console.log(err);
-                    }
+                    if (err) throw err;
                     res.json(notes);
+                    console.log("New note has been saved");
                 });
-            };
-        })
+            }
+        });
     });
 };
