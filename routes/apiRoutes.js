2f35242c6caea89b82eb475e0908519c62fac2a0
Hanh Le <hanhdtle@gmail.com>
2021-03-15 20:32:44 -0700

    added delete option to note

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,12 +3,13 @@ const fs = require('fs');
 // const uuid using npm uuid
 const { v4: uuidv4 } = require('uuid');
 
-//API Routing: 
+// API Routing: 
+// saving and displaying notes
 module.exports = (app) => {
     app.get('/api/notes', (req, res) => {
         fs.readFile('./db/db.json', 'utf8', (err, data) => {
             if (err) throw err;
-            res.json(JSON.parse(data));
+            res.json(JSON.parse(data));0.
         });
     });
 
@@ -30,4 +31,27 @@ module.exports = (app) => {
             }
         });
     });
-};
+
+
+    // deleting note option
+    app.delete('/api/notes/:id', (req, res) => {
+        const noteId = req.params.id;
+        fs.readFile('./db/db.json', 'utf8', (err, data) => {
+            const notes = JSON.parse(data);
+            if (err) { console.err(err) }
+            else {
+                for (let i = 0; i < notes.length; i++) {
+                    if (noteId === notes[i].id) {
+                        notes.splice([i], 1)
+                        fs.writeFile('./db/db.json', JSON.stringify(notes), (err) => {
+                            if (err) throw err;
+                            res.json(notes);
+                            console.log("Note has been deleted");
+                        });
+                    }
+                }
+            }
+        });
+    });
+
+}
