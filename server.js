1d24a494a9b312a9a8c914ba929516d0e3d8d8c0
Hanh Le <hanhdtle@gmail.com>
2021-03-15 19:40:15 -0700

    fixed the path connecting notes.html to index.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 8000;
 
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
+app.use(express.static('./public'));
 app.use(express.json());
 
 require('./routes/apiRoutes')(app);
